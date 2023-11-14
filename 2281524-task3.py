e07f142e6643e9c7c578439604785dd70c170bf9
khaireddine arbouch <66341920+Khaireddine-tech@users.noreply.github.com>
2023-11-15 00:01:06 +0300

    Add files via upload

---
+++ 2281524-task3.py
@@ -0,0 +1,39 @@
+import json
+with open("countries.json", "rt", encoding="utf8") as world:
+    countries = json.load(world)
+
+# Creating a capitalPopulations dictionary
+capitalPopulations = dict()
+
+# Calculate the total population of capitals for each continent
+for country in countries:
+    continent = country["continent"]
+    capitalPopulation = 0
+    
+    # Find the population of the capital city
+    for city in country["cities"]:
+        if city["id"] == country["capital"]:
+            capitalPopulation = city["population"]
+            break
+    
+    # Skip continents with capitalPopulation == 0
+    if capitalPopulation == 0:
+        continue
+
+    # Update the total population for the continent
+    if continent not in capitalPopulations:
+        capitalPopulations[continent] = 0
+    capitalPopulations[continent] += capitalPopulation
+
+# Print the results in alphabetical order of continents
+for continent, population in capitalPopulations.items():
+    print(f"{continent}: {population}")
+
+    """ Output:
+    North America: 19120216
+    Africa: 47714282
+    Europe: 51917214
+    Asia: 99542793
+    South America: 28900527
+    Oceania: 1142110
+    """
