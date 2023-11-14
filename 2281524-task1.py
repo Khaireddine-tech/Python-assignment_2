21dd7486639a027d1ce24fe1761ba24faf1da633
khaireddine arbouch <66341920+Khaireddine-tech@users.noreply.github.com>
2023-11-15 00:00:30 +0300

    Add files via upload

---
+++ 2281524-task1.py
@@ -0,0 +1,19 @@
+import json
+with open("countries.json", "rt", encoding="utf8") as world:
+    countries = json.load(world)
+
+# Set to store unique continents (because a set in Python cannot have duplicate elements)
+continents_set = set()
+
+# Loop through each country and add its continent to the set
+for country in countries:
+    continents_set.add(country["continent"])
+
+# Convert the set to a list
+continents = list(continents_set)
+# Sort the list alphabetically
+continents.sort()
+
+# Print the sorted list of continents
+print(continents) 
+# Output: ['Africa', 'Antarctica', 'Asia', 'Europe', 'North America', 'Oceania', 'South America']
